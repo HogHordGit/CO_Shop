f1927dc85548cf9a33091f2a297773f6196a2871
Volodymyr <you@example.com>
2025-04-30 19:18:30 +0300

    connected to db

--- src/app/account/account.service.ts
+++ src/app/account/account.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {AccountInterface} from './account-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient) { }
+
+  baseUrl:String = "/api/v1/accounts";
+
+  fetchAllAccounts():Observable<AccountInterface[]> {
+    return this._httpClient.get<AccountInterface[]>(`${this.baseUrl}`);
+  }
 }
