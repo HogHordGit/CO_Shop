f4c053214d060e9e56775cbd7b7e41ec02d9c13b
Volodymyr <you@example.com>
2025-05-05 00:17:34 +0300

    added sofrting for list

--- src/app/admin-panel/admin-list/admin-list.component.ts
+++ src/app/admin-panel/admin-list/admin-list.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component} from '@angular/core';
+import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
@@ -6,10 +6,11 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {AdminPanelService} from '../admin-panel.service';
 import {AdminsInterface} from '../admin-panel-interface';
+import {MatSort, MatSortModule} from '@angular/material/sort';
 
 @Component({
   selector: 'app-admin-list',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatTableModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatTableModule, MatSortModule],
   templateUrl: './admin-list.component.html',
   styleUrl: './admin-list.component.scss'
 })
@@ -19,6 +20,8 @@ export class AdminListComponent implements AfterViewInit{
 
   constructor(private adminPanelService: AdminPanelService) {}
 
+  @ViewChild(MatSort) sort: any;
+
   admin:AdminsInterface = {
     id: 0,
     login: "",
@@ -26,11 +29,21 @@ export class AdminListComponent implements AfterViewInit{
   }
 
   admins:AdminsInterface[] = [];
+  filteredAdmins:AdminsInterface[] = [];
 
   ngAfterViewInit() {
     this.adminPanelService.fetchAllAdmins().subscribe((data) => {
       this.admins = data;
       this.dataSource = new MatTableDataSource<AdminsInterface>(data);
+
+      this.dataSource.sort = this.sort;
     })
+  }
+
+  searchAdmin(input:any) {
+    this.filteredAdmins = this.admins.filter(item => item.login.toLowerCase().includes(input.toLowerCase())
+    || item.id.toString().includes(input));
+
+    this.dataSource = new MatTableDataSource<AdminsInterface>(this.filteredAdmins);
   }
 }
