1335a8c4bd181006c77928a2c0c2f383556b7971
Volodymyr <you@example.com>
2025-05-29 07:40:30 +0300

    added detail page

--- src/app/main-site/site-detail/site-detail.component.ts
+++ src/app/main-site/site-detail/site-detail.component.ts
@@ -3,10 +3,13 @@ import {mainSiteParticularItem} from '../../shared/DbLinks/UrlLinks';
 import {AdminPanelService} from '../../shared/services/admin-panel.service';
 import {ClothesInterface} from '../../shared/types/admin-panel-clothes.interface';
 import {ActivatedRoute} from '@angular/router';
+import {CommonModule, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-site-detail',
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './site-detail.component.html',
   standalone: true,
   styleUrl: './site-detail.component.scss'
@@ -21,6 +24,7 @@ export class SiteDetailComponent implements AfterViewInit{
   ngAfterViewInit() {
     this.adminPanelService.fetchParticularData(Number(this.route.snapshot.paramMap.get("id")), this.baseAdminListURL).subscribe((data) => {
       this.clothItem = data;
+      console.log(this.clothItem);
     })
   }
 }
