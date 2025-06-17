636f086e3b825f1ae028026e166f7a16dfd42cd6
Volodymyr <you@example.com>
2025-06-18 00:25:45 +0300

    safe commit

--- src/app/main-site/site-content/site-content.component.ts
+++ src/app/main-site/site-content/site-content.component.ts
@@ -14,7 +14,9 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
 })
 export class SiteContentComponent implements AfterViewInit{
 
-  baseAdminListURL = adminPanelClothesURL;
+  baseAdminListURL:String = adminPanelClothesURL;
+  flagAllNewGoods:Number = 0;
+  flagAllTopGoods:Number = 0;
 
   constructor(private adminPanelService: AdminPanelService) {}
 
@@ -35,5 +37,24 @@ export class SiteContentComponent implements AfterViewInit{
 
     this.visibleNewClothes = this.filteredNewClothes.slice(0, 4);
     this.visibleTopClothes = this.filteredTopClothes.slice(0, 4);
+  }
+
+  btnEventNew() {
+    if (!this.flagAllNewGoods) {
+      this.visibleNewClothes = this.filteredNewClothes;
+      this.flagAllNewGoods = 1;
+    } else {
+      this.visibleNewClothes = this.filteredNewClothes.slice(0, 4);
+      this.flagAllNewGoods = 0;
+    }
+  }
+  btnEventTop() {
+    if (!this.flagAllTopGoods) {
+      this.visibleTopClothes = this.filteredTopClothes;
+      this.flagAllTopGoods = 1;
+    } else {
+      this.visibleTopClothes = this.filteredTopClothes.slice(0, 4);
+      this.flagAllTopGoods = 0;
+    }
   }
 }
